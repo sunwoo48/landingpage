bd0fe6b4f9ade8db4834cc645ef188bec11c90c1
sunwoo48 <anatado@hotmail.com>
2016-05-02 21:12:23 -0400

    add

--- app/sites/controller/mainctrl.js
+++ app/sites/controller/mainctrl.js
@@ -15,14 +15,14 @@
         title:'Personal',
         price:'Free',
         content:'Just started using awesome module. Great way to boost the hard designing or prototyping process',
-        features:'Per month billed annualy or $250 from month to month',
+        features:'Per month billed annually or $250 from month to month',
         button: 'Get it'
       },
       {
         title:'Agency',
         price:'123',
         content:'Also a perfect tool for creative studios and freelancers',
-        features:'Per month billed annualy or $250 from month to month',
+        features:'Per month billed annually or $250 from month to month',
         button: 'Buy now',
         sign:'$'
       },
@@ -30,7 +30,7 @@
         title:'Unlimited',
         price:'232',
         content:'Living in today\'s metropolitan world of cellular phone, mobile computers',
-        features:'Per month billed annualy or $250 from month to month',
+        features:'Per month billed annually or $250 from month to month',
         button: 'Buy now',
         sign:'$'
       }
